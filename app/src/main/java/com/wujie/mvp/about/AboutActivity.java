3d4ed80ed947d88bdcc05b54b2e630d4894e292d
wujie <wujie@tenda.cn>
2017-03-09 14:00:53 +0800

    1 添加ScrollView自动滚动到末尾的功能

--- app/src/main/java/com/wujie/mvp/about/AboutActivity.java
+++ app/src/main/java/com/wujie/mvp/about/AboutActivity.java
@@ -2,24 +2,52 @@ package com.wujie.mvp.about;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.wujie.mvp.R;
 import com.wujie.mvp.base.BaseActivity;
 import com.wujie.mvp.main.MainActivity;
 import com.wujie.mvp.main.MainContract;
 
-public class AboutActivity extends BaseActivity<AboutContract.Presenter> implements AboutContract.View {
+public class AboutActivity extends BaseActivity<AboutContract.Presenter> implements AboutContract.View,
+    View.OnClickListener{
 
+
+    private HorizontalScrollView mScrollView;
+    private LinearLayout mScrollLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Tag = "AboutActivity";
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-        findViewById(R.id.about_btn).setOnClickListener(new View.OnClickListener() {
+        initView();
+
+    }
+    private void initView() {
+        mScrollView = (HorizontalScrollView) findViewById(R.id.path_scrollview) ;
+        mScrollLayout = (LinearLayout) findViewById(R.id.scroll_layout);
+        findViewById(R.id.about_btn).setOnClickListener(this);
+        findViewById(R.id.add_btn).setOnClickListener(this);
+    }
+
+    private void addBtnView() {
+
+
+        TextView textView = new TextView(this);
+        LinearLayout.LayoutParams  params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        textView.setLayoutParams(params);
+        textView.setText((int)(Math.random() * 100) +"---");
+        mScrollLayout.addView(textView);
+        mScrollView.post(new Runnable() {
             @Override
-            public void onClick(View v) {
-                startActivity(new Intent(AboutActivity.this, MainActivity.class));
+            public void run() {
+                mScrollView.scrollTo(mScrollLayout.getWidth(), 0);
+                Log.d(Tag, "width"+ mScrollLayout.getWidth());
             }
         });
     }
@@ -32,5 +60,17 @@ public class AboutActivity extends BaseActivity<AboutContract.Presenter> impleme
     @Override
     public boolean isViewActive() {
         return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.about_btn:
+                startActivity(new Intent(AboutActivity.this, MainActivity.class));
+                break;
+            case R.id.add_btn:
+                addBtnView();
+                break;
+        }
     }
 }
